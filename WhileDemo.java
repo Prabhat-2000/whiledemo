ff32461d28cab314e0b81095eb002f935af55110
Prabhat Srivastava <102291706+Prabhat-2000@users.noreply.github.com>
2022-08-08 22:12:37 +0530

    Add files via upload

---
+++ WhileDemo.java
@@ -0,0 +1,12 @@
+package dailywork;
+
+public class WhileDemo {
+    public static void main(String[] args){
+
+        int count=1;
+        while(count<10){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
